6acae016b47e1a7f08c831e7602f9e441e081966
free6k <me@free6k.ru>
2020-04-10 13:31:06 +0300

    First version

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# filerotate
+#File Rotate
+A tiny script for rotation big files. by next limits: size,count
+
+##Install
+
+`git checkout git@github.com:free6k/filerotate.git`
+
+##How it use
+
+`python filerotate.py -f '/backups/dump_*.sql.gz'' -c 30 -s 100M`
+
+##Params
+
+- -f --file - pattern for find files to rotate
+- -c --count - minimal count files, unnecessary will be delete
+- -s --size - max summary size, unnecessary will be delete
+
+##Example
+
+python filerotate.py -f '/backups/dump_*.sql.gz'' -c 30 -s 100M
+
+script will find files by <pattern: /backups/dump_*.sql.gz>  
+next if count > 30, will be delete old files over 30  
+after summary files size > 100M, will be delete old files until summary size will be equal 100M
---
+++ filerotate.py
@@ -0,0 +1,122 @@
+import sys, os, glob, getopt, time, re, hashlib
+
+PID = str(os.getpid())
+PIDFILE = "/tmp/FILEROTATE-RUNNING-.pid"
+
+def can_it_run():
+  # Check wether lock PIDFILE exists
+  if os.path.isfile(PIDFILE):
+    return False
+  else:
+    return True
+
+def run(argv):
+  filepattern = count = maxsize = interval = None
+
+  try:
+    opts, args = getopt.getopt(argv,"hf:c:s:i:",["file=","count=","size=","interval="])
+  except getopt.GetoptError:
+    print 'rotate.py -f /test/test*.log -c 10 -s 100M'
+    sys.exit(2)
+
+  for opt, arg in opts:
+    if opt == '-h':
+       print 'rotate.py -f <filepattern> -c <max count: more 0> -s <max summary in 1M 1K 1G 1T>'
+       sys.exit()
+    elif opt in ("-f", "--file"):
+       filepattern = arg
+    elif opt in ("-c", "--count"):
+       count = int(arg)
+    elif opt in ("-s", "--size"):
+       maxsize = arg.lower()
+       size = int(re.sub("\D", "", maxsize));
+
+       if maxsize.find('k') != -1:
+        maxsize = int(size) * 1024.0
+       elif maxsize.find('m') != -1:
+        maxsize = int(size) * 1024.0 * 1024.0
+       elif maxsize.find('g') != -1:
+        maxsize = int(size) * 1024.0 * 1024.0 * 1024.0
+       elif maxsize.find('t') != -1:
+        maxsize = int(size) * 1024.0 * 1024.0 * 1024.0 * 1024.0
+    elif opt in ("-i", "--interval"):
+        interval = arg.lower()
+
+  if not((filepattern and (count or maxsize))):
+    print 'One of the parameters must be: -s <maxsize megabytes> or -c <count>'
+    sys.exit(2)
+
+  if not(count):
+    count = 0
+
+  open(PIDFILE, 'w').write(PID);
+
+  try:
+    files = glob.glob(filepattern)
+    files.sort(key=os.path.getmtime, reverse = True)
+
+    if count > 0:
+      print 'Found %d files, last %s' % (len(files), time.strftime('%b %d %Y %H:%M:%S', time.localtime(os.path.getmtime(files[0]))))
+
+      oldfiles = files[count:]
+
+      for oldfile in oldfiles:
+        os.unlink(oldfile)
+
+      if oldfiles:
+        print 'Deleted %d files by count limit' % len(oldfiles)
+
+      files = files[:len(files) - len(oldfiles)]
+
+    if maxsize > 0:
+      total_size = 0
+      files_with_size = {}
+
+      for file in files:
+        files_with_size[file] = os.path.getsize(file)
+        total_size += files_with_size[file]
+
+      print 'Total size %s limit %s' % (sizeof_fmt(total_size), sizeof_fmt(maxsize))
+
+      if total_size > maxsize:
+        realsize = 0
+        count = 0
+
+        for file in files_with_size:
+          realsize += files_with_size[file]
+
+          if realsize > maxsize:
+            count += 1
+
+        oldfiles = files[count:]
+
+        for oldfile in oldfiles:
+          os.unlink(oldfile)
+
+        if oldfiles:
+          print 'Deleted %d files by maxsize limit' % len(oldfiles)
+
+        files = files[:len(files) - len(oldfiles)]
+
+    else:
+      print 'Found %d files' % (len(files))
+
+
+  #  for file in files:
+
+  finally:
+    os.unlink(PIDFILE)
+
+def sizeof_fmt(num, suffix='B'):
+  for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+      if abs(num) < 1024.0:
+          return "%3.1f%s%s" % (num, unit, suffix)
+      num /= 1024.0
+  return "%.1f%s%s" % (num, 'Yi', suffix)
+
+if __name__ == '__main__':
+  if can_it_run():
+    run(sys.argv[1:]);
+  else:
+    old_pid = ''.join(file("RUNNING.pid"))
+    print "Script already running under PID %s, skipping execution." % old_pid
